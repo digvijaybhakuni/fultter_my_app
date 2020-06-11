31c0678f18d0c934df51a424b90e72bec4ca2e94
Digvijay <digvijayb@icloud.com>
2020-06-11 23:04:51 +0530

    first commit

---
+++ lib/camera_action.dart
@@ -0,0 +1,143 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:my_app/view_camera_preview.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+
+class TakePictureScreen extends StatefulWidget {
+  @override
+  _TakePictureScreenState createState() => _TakePictureScreenState();
+}
+
+class _TakePictureScreenState extends State<TakePictureScreen> {
+  CameraController controller;
+  List cameras;
+  int selectedCameraIdx;
+  String imagePath;
+
+  @override
+  void initState() {
+    super.initState();
+
+    availableCameras().then((camerasList) {
+      this.cameras = camerasList;
+      if (this.cameras.length > 0) {
+        setState(() {
+          selectedCameraIdx = 0;
+        });
+        this._initCameraController(this.cameras[selectedCameraIdx]);
+      } else {
+        print("No camera available");
+      }
+    }).catchError((err) => print(err));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    this.controller?.dispose();
+  }
+
+  Future<String> getImagePath() async {
+    return join(
+      (await getTemporaryDirectory()).path,
+      '${DateTime.now()}.png',
+    );
+  }
+
+  Future _initCameraController(CameraDescription cameraDescription) async {
+    if (this.controller != null) {
+      await controller?.dispose();
+    }
+
+    this.controller =
+        CameraController(cameraDescription, ResolutionPreset.medium);
+
+    this.controller.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+
+      if (this.controller.value.hasError) {
+        print("this.controller.value.hasError");
+        print('Camera error ${controller.value.errorDescription}');
+      }
+    });
+
+    try {
+      await this.controller.initialize();
+    } on CameraException catch (e) {
+      print("CameraException ${e.description}");
+      // _showCameraException(e);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Widget _cameraPreviewWidget() {
+    if (this.controller == null || !this.controller.value.isInitialized) {
+      return Center(
+        child: const Text(
+          'Loading',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+      );
+    }
+
+    final cameraView = AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: CameraPreview(controller));
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          cameraView,
+          RaisedButton(
+            child: Text("Take Pick"),
+            onPressed: () async {
+              print("clicked");
+              final path = await this.getImagePath();
+              await controller.takePicture(path);
+              this.imagePath = path;
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DisplayPictureScreen(imagePath: path),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // return _cameraPreviewWidget();
+    return Scaffold(
+      body: _cameraPreviewWidget(),
+      appBar: AppBar(
+        title: Text('Camera'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.backspace),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+
+import 'package:my_app/camera_action.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:my_app/view_camera_preview.dart';
+
+void main() => runApp(MyApp());
+
+@immutable
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  var routes = <String, WidgetBuilder>{
+    '/': (context) => MyHomePage(title: 'DB Works X'),
+    '/camera': (context) => TakePictureScreen(),
+  };
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+        ),
+        initialRoute: '/',
+        routes: routes);
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final SnackBar snackBar = const SnackBar(content: Text('Showing Snackbar '));
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 1;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: widget.scaffoldKey,
+      appBar: AppBar(
+        title: Text(widget.title),
+        //backgroundColor: Colors.amber,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              widget.scaffoldKey.currentState.showSnackBar(widget.snackBar);
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.navigate_next),
+            tooltip: 'Next page',
+            onPressed: () {
+              print('Next Page');
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Counter:',
+                    style: Theme.of(context).textTheme.headline3,
+                  ),
+                  Text('$_counter',
+                      style: Theme.of(context).textTheme.headline4)
+                ],
+              ),
+            ),
+            RaisedButton.icon(
+              onPressed: () {
+                print('try to open camera');
+                // Navigator.pushNamed(context, '/camera');
+                optionsCameraDialogBox(context);
+              },
+              icon: Icon(Icons.camera,
+                  color: Theme.of(context).accentIconTheme.color),
+              label: Text("Get Image",
+                  style: Theme.of(context).accentTextTheme.button),
+              color: Theme.of(context).accentColor,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        backgroundColor: Theme.of(context).accentColor,
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+Future takeImageWithCamera(context) async {
+  final image = await ImagePicker().getImage(source: ImageSource.camera);
+  // final image = await ImagePicker.pickImage(source: ImageSource.camera);
+  print(image);
+  Navigator.pop(context);
+  openImageTaken(context, image.path);
+}
+
+Future takeImageWithGallery(context) async {
+  final image = await ImagePicker().getImage(source: ImageSource.gallery);
+  print(image);
+  Navigator.pop(context);
+  openImageTaken(context, image.path);
+}
+
+void openImageTaken(context, path) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => DisplayPictureScreen(imagePath: path),
+    ),
+  );
+}
+
+Future<void> optionsCameraDialogBox(context) {
+  return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: new SingleChildScrollView(
+            child: new ListBody(
+              children: <Widget>[
+                GestureDetector(
+                  child: new Text('My Take a picture'),
+                  onTap: () async {
+                    final _ = await Navigator.pushNamed(context, '/camera');
+                    print(_);
+                    Navigator.pop(context);
+                    return _;
+                  },
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                ),
+                GestureDetector(
+                    child: new Text('Take a picture'),
+                    onTap: () => takeImageWithCamera(context)),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                ),
+                GestureDetector(
+                  child: new Text('Select from gallery'),
+                  onTap: () => takeImageWithGallery(context),
+                ),
+              ],
+            ),
+          ),
+        );
+      });
+}
